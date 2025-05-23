7dae8e34c341cc8a659eee83bead474d1d5e97ea
Caroline Clara <caroline.clara@sga.pucminas.br>
2025-05-23 17:55:44 -0300

    Aumenta tempo de duração da notificação; adiciona tipo de notificação warning.

--- src/mfe/mfe-host/src/composables/useNotify.ts
+++ src/mfe/mfe-host/src/composables/useNotify.ts
@@ -2,8 +2,20 @@ import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 
 const notyf = new Notyf({
-  duration: 3000,
+  duration: 5000,
   position: { x: 'right', y: 'bottom' },
+  types: [
+    {
+      type: 'warning',
+      background: '#FBBF24',
+      icon: {
+        tagName: 'i',
+        className: 'material-icons',
+        text: 'error',
+        color: 'white',
+      },
+    },
+  ],
 });
 
 export function useNotify() {
